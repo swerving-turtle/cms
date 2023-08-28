41ee86024525ebc6f9ed43fc9db6356fed9a46ed
Keonwoo Moon <swervingturtle@gmail.com>
2023-08-28 10:40:32 +0900

    postgresql로 db 변경

--- educa/settings.py
+++ educa/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'educa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'educa_db',
+        'USER': 'educa',
+        'PASSWORD': '1234',
     }
 }
 
